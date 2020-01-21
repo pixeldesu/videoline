b1c25bae574907d1f8ea4d9b0a06e5b0ca21cf1e
Andreas Nedbal <andy@pixelde.su>
2020-01-21 21:50:29 +0100

    Add initial PWA settings and assets
    Fixes #5

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
+import './registerServiceWorker';
 
 Vue.config.productionTip = false;
 
--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,18 @@ module.exports = {
   transpileDependencies: [
     'vuetify',
   ],
+  pwa: {
+    name: 'videoline',
+    themeColor: '#1976d2',
+    msTileColor: '#1976d2',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black-translucent',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: 'src/serviceWorker.js',
+      swDest: 'service-worker.js'
+    }
+  }
 };
