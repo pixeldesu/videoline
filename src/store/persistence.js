c88753e92a10b473e8aeb8b0a6401064317990d3
Andreas Nedbal <andy@pixelde.su>
2020-01-28 22:58:44 +0100

    persist configuration on state changes

--- src/store/persistence.js
+++ src/store/persistence.js
@@ -3,6 +3,7 @@ import VuexPersistence from 'vuex-persist';
 const ALLOWED_MUTATIONS = [
   'TOGGLE_DRAWER',
   'SELECT_CHANNEL',
+  'SET_CONFIG',
   'SET_SORTING_PROPERTY',
   'SET_SORTING_ORDER',
 ];
@@ -10,6 +11,7 @@ const ALLOWED_MUTATIONS = [
 export default new VuexPersistence({
   storage: window.localStorage,
   reducer: (state) => ({
+    config: state.config,
     drawer: state.drawer,
     selectedChannel: state.selectedChannel,
     sorting: state.sorting,
