f87fce9fa80b4ad0fc0b81c7967473c78bd4d7d3
Andreas Nedbal <andy@pixelde.su>
2020-01-31 23:49:08 +0100

    added support for choosing channel usernames or IDs
    also added hints so finding those identifiers on YouTube is easier

--- src/modules/feed.js
+++ src/modules/feed.js
@@ -1,5 +1,5 @@
 import * as xml2js from 'xml2js';
 
-export default function loadFeed(corsProxyUrl, channelID) {
-  return fetch(`${corsProxyUrl}https://www.youtube.com/feeds/videos.xml?channel_id=${channelID}`).then((response) => response.text()).then((xml) => xml2js.parseStringPromise(xml)).then((result) => result.feed);
+export default function loadFeed(corsProxyUrl, channelID, type = 'channel_id') {
+  return fetch(`${corsProxyUrl}https://www.youtube.com/feeds/videos.xml?${type}=${channelID}`).then((response) => response.text()).then((xml) => xml2js.parseStringPromise(xml)).then((result) => result.feed);
 }
