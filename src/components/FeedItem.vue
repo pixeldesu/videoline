cab7c71da0a490db7d5b188eda27c0501e920c63
Andreas Nedbal <git@pixelde.su>
2023-05-02 22:43:42 +0200

    add initial FeedItem implementation

---
+++ src/components/FeedItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card>
+    <v-img
+      class="white--text align-end"
+      height="200px"
+      >
+      <v-chip
+        class="ma-2 float-right"
+        color="grey darken-4 white--text transparent"
+        label
+      >
+        chip content
+      </v-chip>
+    </v-img>
+    <v-card-title class="subtitle-2 text-truncate d-block">Title</v-card-title>
+    <v-card-subtitle>Subtitle</v-card-subtitle>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+
+</script>
