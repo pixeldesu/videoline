de13f48040b7249ef4befe93817db268de733bba
Andreas Nedbal <git@pixelde.su>
2023-05-02 22:43:32 +0200

    add initial FeedContainer implementation

---
+++ src/components/FeedContainer.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-row v-if="loading">
+    <v-col md="3" v-for="n in 12" v-bind:key="n">
+      <FeedItem/>
+    </v-col>
+  </v-row>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import FeedItem from './FeedItem.vue'
+
+const loading = ref(true)
+</script>
