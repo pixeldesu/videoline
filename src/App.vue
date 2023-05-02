59be80f22c9450961b23924e986a71973cab0fb4
Andreas Nedbal <git@pixelde.su>
2023-05-02 20:28:08 +0200

    initialize new Vite project

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,30 @@
-<template>
-  <v-app>
-    <AppBar/>
-    <AppNavigationDrawer/>
-
-    <v-content>
-      <v-container class="safe-area-ios-container">
-        <FeedSortFilter/>
-        <FeedContainer v-if="videos" :loading="loading" :entries="videos"/>
-        <v-snackbar
-          bottom
-          color="error"
-          value="true"
-          v-for="(errorMessage, index) in errorMessages" :key="index"
-          >
-          {{ errorMessage }}
-        </v-snackbar>
-      </v-container>
-    </v-content>
-  </v-app>
-</template>
-
-<script>
-import { mapState } from 'vuex';
-
-import AppBar from './components/AppBar.vue';
-import AppNavigationDrawer from './components/AppNavigationDrawer.vue';
-import FeedContainer from './components/FeedContainer.vue';
-import FeedSortFilter from './components/FeedSortFilter.vue';
-
-export default {
-  name: 'App',
-
-  components: {
-    AppBar,
-    AppNavigationDrawer,
-    FeedContainer,
-    FeedSortFilter,
-  },
-
-  computed: mapState([
-    'config',
-    'errorMessages',
-    'loading',
-    'videos',
-  ]),
-
-  async created() {
-    const self = this;
-
-    this.$store.dispatch('getChannels').then(() => {
-      this.$store.dispatch('getVideos');
-    });
-
-    function respectTheme() {
-      let { darkTheme } = self.$store.state.config;
-      if (darkTheme === 0) {
-        darkTheme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-      }
-      self.$vuetify.theme.dark = darkTheme;
-    }
-
-    if (window.matchMedia) {
-      window.matchMedia('(prefers-color-scheme: dark)').addListener(respectTheme);
-    }
-
-    this.$store.watch(
-      (state) => state.config.darkTheme,
-      respectTheme,
-    );
-
-    respectTheme();
-  },
-};
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style>
-.safe-area-ios {
-  padding-top: env(safe-area-inset-top);
-}
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
 
-.safe-area-ios-container {
-  padding-top: calc(2.5 * env(safe-area-inset-top));
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
