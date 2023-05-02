4368857b53da62c2215c88bf2dc7723ab22424af
Andreas Nedbal <git@pixelde.su>
2023-05-02 22:01:34 +0200

    configure Vuetify asset transformation

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vuetify from 'vite-plugin-vuetify'
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({ 
+      template: { transformAssetUrls }
+    }),
     vuetify({ autoImport: true })
   ],
 })
